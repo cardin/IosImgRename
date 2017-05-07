9774cce4607e35150db994b36f81f4a7bf400950
Cardin LJH <cardinleejh@gmail.com>
2017-05-08 00:02:12 +0800

    Fixed linting

--- ios_img_rename.py
+++ ios_img_rename.py
@@ -1,15 +1,17 @@
+''' Renames images that are from IOS camera '''
 from tkinter import Tk, Frame, Label, Scrollbar, Listbox, Button, messagebox, \
-	BOTH, EXTENDED, RIGHT, LEFT, END
+	EXTENDED, RIGHT, LEFT, END
 import tkinter.filedialog as filedialog
 import os
 import re
 
 class App(Frame):
+	''' The application model '''
 	DIRECTORY = r'C:\Users\Cardin\OneDrive\YH and JH memories'
 	WIDTH = 400
 	HEIGHT = 310
 
-	FILE_PATTERN = re.compile('^(\d+)(_\d+_iOS\.(?:jpg|jpeg|png))$')
+	FILE_PATTERN = re.compile(r'^(\d+)(_\d+_iOS\.(?:jpg|jpeg|png))$')
 
 	def __init__(self, parent):
 		''' Constructor '''
@@ -21,18 +23,19 @@ class App(Frame):
 		# main loop
 		self.dir_selection()
 		self.init_ui()
-		
+
 	def dir_selection(self):
 		''' Get the directory to run '''
-		self.desired_dir = desired_dir = filedialog.askdirectory(initialdir = App.DIRECTORY, title = 'Please select a directory to check for')
-		
-		if len(desired_dir) == 0:
+		self.desired_dir = desired_dir = \
+			filedialog.askdirectory(initialdir=App.DIRECTORY, title='Please select a directory to check for')
+
+		if desired_dir is None or not desired_dir: # empty string
 			messagebox.showinfo('Alert', 'No directory selected.\nApp will be terminating')
 			exit(0)
 
 	def init_ui(self):
 		''' Get the UI up '''
-		dir = self.desired_dir
+		target_dir = self.desired_dir
 
 		self.parent.title('IOS Image Rename')
 		self.pack() # as tight as possible
@@ -45,19 +48,20 @@ class App(Frame):
 		list_frame = Frame(self)
 		list_frame.pack(pady=5) # as tight as possible
 		scrollbar = Scrollbar(list_frame, orient='vertical')
-		self.listnodes = listnodes = Listbox(list_frame, selectmode=EXTENDED, yscrollcommand=scrollbar.set,\
-			width=40, height=15) # width = #char, height = #lines
+		self.listnodes = listnodes = Listbox(list_frame, selectmode=EXTENDED, \
+			yscrollcommand=scrollbar.set, width=40, height=15) # width = #char, height = #lines
 		scrollbar.config(command=listnodes.yview)
 
 		scrollbar.pack(side=RIGHT, fill='y') # as long as needed
 		listnodes.pack(side=LEFT, fill='y') # as long as stated in constructor
 
 		## Iterate though the directory
-		for file in os.listdir(dir):
+		for file in os.listdir(target_dir):
 			if App.FILE_PATTERN.search(file):
 				listnodes.insert(END, file)
 		if listnodes.size() == 0:
-			messagebox.showwarning('No files', 'No matching files found in {0}.\nApp will be terminating'.format(dir))
+			messagebox.showwarning('No files', \
+				'No matching files found in {0}.\nApp will be terminating'.format(target_dir))
 			exit(0)
 
 		# Button Frame - Ok button
@@ -81,7 +85,7 @@ class App(Frame):
 
 	def rename(self):
 		''' Begin renaming the given list of files '''
-		dir = self.desired_dir
+		target_dir = self.desired_dir
 		listnodes = self.listnodes
 
 		# disable button
@@ -93,26 +97,26 @@ class App(Frame):
 		target_files = unselected - selected
 
 		for file in target_files:
-	
 			# get the portions we need
 			pattern_result = self.FILE_PATTERN.match(file)
 			date_prefix = pattern_result.group(1)
 
 			# create our new names
 			newdate_prefix = "{0}-{1}-{2}".format(date_prefix[:4], date_prefix[4:6], date_prefix[6:])
-			new_name = os.path.join(dir, newdate_prefix + pattern_result.group(2))
+			new_name = os.path.join(target_dir, newdate_prefix + pattern_result.group(2))
 
 			# rename
-			old_name = os.path.join(dir, file)
+			old_name = os.path.join(target_dir, file)
 			os.rename(old_name, new_name)
 
-		messagebox.showinfo('Done', 'Processing is completed!\n{0} file[s] renamed.'.format(len(target_files)))
+		messagebox.showinfo('Done', \
+			'Processing is completed!\n{0} file[s] renamed.'.format(len(target_files)))
 		self.parent.quit()
 
 def main():
 	''' Main function '''
 	root = Tk()
-	app = App(root)
+	_ = App(root)
 	root.mainloop()
 
 if __name__ == "__main__":
